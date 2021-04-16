4072792f4f74bfd8074d0facb1415a2ec66be31c
Nicolas Serafini <nicolas.serafini@tunubi.com>
2021-04-16 19:08:17 -0300

    feat: Add config

---
+++ config.py
@@ -0,0 +1,13 @@
+import os
+
+class Config(object):
+    POSTGRES_USER = os.environ.get("POSTGRES_USER")
+    POSTGRES_PASSWORD = os.environ.get("POSTGRES_PASSWORD")
+    POSTGRES_HOST = os.environ.get("POSTGRES_HOST")
+    POSTGRES_PORT = os.environ.get("POSTGRES_PORT")
+    POSTGRES_DB = os.environ.get("POSTGRES_DB")
+    SQLALCHEMY_DATABASE_URI = (
+        f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}"
+        f"@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
+    )
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
